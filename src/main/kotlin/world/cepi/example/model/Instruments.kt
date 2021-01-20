2e70233d18b781abd7f074fa4a81740804dc58f6
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-20 16:53:28 +0000

    Make Instruments take a Sound as a parameter

--- src/main/kotlin/world/cepi/example/model/Instruments.kt
+++ src/main/kotlin/world/cepi/example/model/Instruments.kt
@@ -1,13 +1,30 @@
 package world.cepi.example.model
 
-enum class Instrument {
-    PIANO, DOUBLE_BASE, BASE, SNARE_DRUM, CLICK, GUITAR, FLUTE, BELL, CHIME, XYLOPHONE, IRON_XYLOPHONE, COW_BELL, DIDGERIDOO, BIT, BANJO, PLING;
+import net.minestom.server.sound.Sound
+
+enum class Instrument(val sound: Sound) {
+    PIANO(TODO()),
+    DOUBLE_BASE(Sound.BLOCK_NOTE_BLOCK_BASS),
+    BASE_DRUM(Sound.BLOCK_NOTE_BLOCK_BASEDRUM),
+    SNARE_DRUM(Sound.BLOCK_NOTE_BLOCK_BASEDRUM),
+    CLICK(Sound.BLOCK_NOTE_BLOCK_HAT),
+    GUITAR(Sound.BLOCK_NOTE_BLOCK_GUITAR),
+    FLUTE(Sound.BLOCK_NOTE_BLOCK_FLUTE),
+    BELL(Sound.BLOCK_NOTE_BLOCK_BELL),
+    CHIME(Sound.BLOCK_NOTE_BLOCK_CHIME),
+    XYLOPHONE(Sound.BLOCK_NOTE_BLOCK_XYLOPHONE),
+    IRON_XYLOPHONE(Sound.BLOCK_NOTE_BLOCK_IRON_XYLOPHONE),
+    COW_BELL(Sound.BLOCK_NOTE_BLOCK_COW_BELL),
+    DIDGERIDOO(Sound.BLOCK_NOTE_BLOCK_DIDGERIDOO),
+    BIT(Sound.BLOCK_NOTE_BLOCK_BIT),
+    BANJO(Sound.BLOCK_NOTE_BLOCK_BANJO),
+    PLING(Sound.BLOCK_NOTE_BLOCK_PLING);
 
     companion object {
         private val instrumentBytes: Map<Byte, Instrument> = mapOf(
             0.toByte() to PIANO,
             1.toByte() to DOUBLE_BASE,
-            2.toByte() to BASE,
+            2.toByte() to BASE_DRUM,
             3.toByte() to SNARE_DRUM,
             4.toByte() to CLICK,
             5.toByte() to GUITAR,
