7a333cf51eb6e68df67a1cae6ad2a50f6779be91
LeoDog896 <LeoDog896@hotmail.com>
2021-06-12 17:57:57 -0400

    Don't use Int wrapper in mutable map

--- src/main/kotlin/world/cepi/orchestra/api/data/formatted/SongMap.kt
+++ src/main/kotlin/world/cepi/orchestra/api/data/formatted/SongMap.kt
@@ -1,11 +1,12 @@
 package world.cepi.orchestra.api.data.formatted
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import net.minestom.server.entity.Player
 import world.cepi.orchestra.api.SongPlayerInstance
 
 class SongMap {
 
-    val map = mutableMapOf<Int, MutableMap<Int, SongNote>>()
+    val map: MutableMap<Int, MutableMap<Int, SongNote>> = Int2ObjectOpenHashMap()
 
     operator fun get(x: Int, y: Int): SongNote? {
         if (map[x] == null) return null
@@ -15,7 +16,7 @@ class SongMap {
     }
 
     operator fun set(x: Int, y: Int, note: SongNote) {
-        if (map[x] == null) map[x] = mutableMapOf()
+        if (map[x] == null) map[x] = Int2ObjectOpenHashMap()
 
         map[x]!![y] = note
     }
