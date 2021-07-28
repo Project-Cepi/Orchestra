06de16a33bb5503116d9363e81f2f9725642b83c
LeoDog896 <LeoDog896@hotmail.com>
2021-07-27 23:35:13 -0400

    Add ArgumentSound

--- src/main/kotlin/world/cepi/orchestra/command/PlaySoundCommand.kt
+++ src/main/kotlin/world/cepi/orchestra/command/PlaySoundCommand.kt
@@ -9,15 +9,31 @@ import net.minestom.server.entity.Player
 import net.minestom.server.utils.Vector
 import net.minestom.server.utils.entity.EntityFinder
 import net.minestom.server.utils.location.RelativeVec
+import net.minestom.server.utils.math.FloatRange
 import world.cepi.kstom.command.addSyntax
+import kotlin.random.Random
 
 object PlaySoundCommand : Command("playsound") {
 
     init {
 
-        val sound = ArgumentType.ResourceLocation("sound")
-        val volume = ArgumentType.Float("volume").setDefaultValue(1f)
-        val pitch = ArgumentType.Float("pitch").setDefaultValue(1f)
+        val sound = ArgumentSound("sound")
+
+        val volume = ArgumentType.FloatRange("volume").setDefaultValue(FloatRange(1f, 1f)).map {
+
+            if (it.minimum == it.maximum)
+                return@map it.minimum
+
+            Random.Default.nextDouble(it.minimum.toDouble(), it.maximum.toDouble()).toFloat()
+        }
+
+        val pitch = ArgumentType.FloatRange("pitch").setDefaultValue(FloatRange(1f, 1f)).map {
+
+            if (it.minimum == it.maximum)
+                return@map it.minimum
+
+            Random.Default.nextDouble(it.minimum.toDouble(), it.maximum.toDouble()).toFloat()
+        }
 
         val targets = ArgumentType.Entity("target").setDefaultValue {
             EntityFinder().apply {
@@ -25,14 +41,20 @@ object PlaySoundCommand : Command("playsound") {
             }
         }
 
-        val location = ArgumentType.RelativeVec3("source").setDefaultValue(RelativeVec(Vector(.0, .0, .0), true, true, true))
+        val location = ArgumentType.RelativeVec3("source")
+            .setDefaultValue(RelativeVec(Vector(.0, .0, .0), true, true, true))
 
         val soundCategory = ArgumentType.Enum("category", Sound.Source::class.java).also {
             it.setFormat(ArgumentEnum.Format.LOWER_CASED)
         }.setDefaultValue(Sound.Source.MASTER)
 
         addSyntax(sound, volume, pitch, targets, location, soundCategory) {
-            val createdSound = Sound.sound(Key.key(context[sound]), context[soundCategory], context[volume], context[pitch])
+            val createdSound = Sound.sound(
+                context[sound],
+                context[soundCategory],
+                context[volume], context[pitch]
+            )
+
             context[targets].find(sender).forEach {
                 (it as? Player)?.playSound(createdSound)
             }
